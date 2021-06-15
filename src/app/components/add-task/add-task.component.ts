ad34efc13bacc1de1de9549c8940e72cc65fa739
ericcapiz <ericcapiz@gmail.com>
2021-06-15 17:13:05 -0500

    can toggle the add btn to open form; close to close form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
 import {Task} from '../../Task';
+import { UiService } from '../../services/ui.service';
 
 @Component({
   selector: 'app-add-task',
@@ -11,7 +13,11 @@ export class AddTaskComponent implements OnInit {
 text!: string;
 day!: string;
 reminder:boolean = false;
-  constructor() { }
+showAddTask!: boolean;
+subscription!: Subscription;
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value) )
+  }
 
   ngOnInit(): void {
   }
